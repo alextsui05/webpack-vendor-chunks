c1158692a2e4bdbd37d24586eac7d0d8df0ee241
Alex Tsui <alextsui@pm.me>
2020-07-14 22:14:55 +0000

    :ok_hand: Add vendor cache group config

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,23 @@ const path = require('path');
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'main.js',
+    filename: '[name].[contenthash].js',
     path: path.resolve(__dirname, 'dist'),
+  },
+  optimization: {
+    runtimeChunk: 'single',
+    splitChunks: {
+      chunks: 'all',
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name(module) {
+            const packageName = module.context.match(/[\\/]node_modules[\\/](.*?)([\\/]|$)/)[1];
+            return `npm.${packageName.replace('@', '')}`;
+          },
+          priority: 10,
+        },
+      },
+    },
   },
 };
